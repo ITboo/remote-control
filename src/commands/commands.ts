9615fff0f5c90901c0c517feccc7e1e10f00e05f
ITboo <annaacdc@tut.by>
2023-01-19 22:49:19 +0300

    init: start websockets

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,7 +1,7 @@
 import { mouse, left, right, up, down, Point, straightTo, Button } from "@nut-tree/nut-js";
 //import Jimp from 'jimp';
 
-const draw = async (x: number, y: number) => {
+export const draw = async (x: number, y: number) => {
     await mouse.move(left(x)); // Move mouse left <- mouse_left {x px}
     await mouse.move(up(y)); // Move mouse up <- mouse_up {y px}
     await mouse.move(right(x));// Move mouse right <- mouse_right {x px}
@@ -16,12 +16,18 @@ const draw = async (x: number, y: number) => {
 
 
 // Draw circle with pushed left button:<- draw_circle {px}
-export const drawCircle = async (radius: number) => { };
+export const drawCircle = async (radius: number) => {
+    await mouse.releaseButton(Button.LEFT);
+};
+
 // Draw rectangle with pushed left button:<- draw_rectangle {px} {px}
-export const drawRectangle = async (width: number, length: number) => { };
+export const drawRectangle = async (width: number, length: number) => {
+    await mouse.releaseButton(Button.LEFT);
+};
+
 // Draw square with pushed left button: <- draw_square {px}
-export const drawSquare = async (width: number) => { };
-
-
+export const drawSquare = async (width: number) => {
+    await mouse.releaseButton(Button.LEFT);
+};
 
 // Make print screen command and send image (a base64 buffer of the 200 px square around the mouse position): <- prnt_scrn -> prnt_scrn {base64 string (png buf)}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { httpServer } from "./http_server/index.js";
-import { WebSocketServer } from "ws"; // websocket соединение
+import { WebSocketServer } from "ws";
 //import dotenv from 'dotenv';
-import { mouse } from "@nut-tree/nut-js";
-
 
 const HTTP_PORT = 8181;
+const WSS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+// websocket соединение
+const wss = new WebSocketServer({port: WSS_PORT});
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+});
+/*
+process.on("SIGNINT", () => {
+    console.log("Closed");
+    wss.close();
+    process.exit(0);
+  });*/
