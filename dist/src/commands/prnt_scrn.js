9032ee9a80665c1042f7d21601f35651c9b50059
ITboo <annaacdc@tut.by>
2023-01-23 21:02:08 +0300

    refactor: ignore dist

--- dist/src/commands/prnt_scrn.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//import Jimp from 'jimp';
-//.getBufferAsync(Jimp.MIME_PNG)
-// Make print screen command and send image (a base64 buffer of the 200 px square around the mouse position): <- prnt_scrn -> prnt_scrn {base64 string (png buf)}
-//# sourceMappingURL=prnt_scrn.js.map
--- dist/src/common/errors.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COMMAND_ERROR = void 0;
-exports.COMMAND_ERROR = console.log(`Command is not found`);
-//# sourceMappingURL=errors.js.map
